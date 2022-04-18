7de546130a72acf46c7d2e812263c362c8649f45
Jason Kim <actcrazy@gmail.com>
2022-04-18 17:00:03 +0900

    add files

---
+++ src/Comments.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { useData } from './data';
+
+export default function Comments() {
+  console.log("test11")
+  const comments = useData();
+  console.log("test22")
+  return (
+    <>
+      {comments.map((comment, i) => (
+        <p className="comment" key={i}>
+          {comment}
+        </p>
+      ))}
+    </>
+  );
+}
